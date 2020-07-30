13effdfded67f12b347d47dd741bff8f12769415
Sergey R <romanov@bcstech.ru>
2020-07-30 06:45:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Start
+Типовое наполнение для старта проекта.
+Подключено все, что нужно.
+
+* Сначала инициализировать:  
+nmp init
+
+* Скопировать все файлы
+
+* Выполнить:
+```
+npm install --save-dev webpack webpack-cli html-webpack-plugin copy-webpack-plugin clean-webpack-plugin mini-css-extract-plugin sass-loader sass css-loader babel-loader @babel/core @babel/preset-env cross-env webpack-dev-server @babel/polyfill eslint eslint-loader babel-eslint eslint-config-google
+```
+* В настройках WebStorm включить ESLint (ESLint - Manual ESLint configuration - выбрать ESLint Package, Configuration file)
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      }
+    }
+  ];
+
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+
+  return loaders;
+};
+
+module.exports = {
+  // где лежат исходники, за которыми будет следить webpack
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: [
+    '@babel/polyfill',
+    './index.js'
+  ],
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 4200,
+    hot: isDev
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      minify: isProd,
+      collapseWhitespace: isProd
+    }),
+    new CopyPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'src/favicon.ico'),
+        to: path.resolve(__dirname, 'dist')
+      }]
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders()
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      },
+    ],
+  },
+};
